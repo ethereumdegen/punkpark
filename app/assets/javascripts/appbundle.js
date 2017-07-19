11f7ecaa47927c12d1233ceedceaf3979241d3bc
andy <andy@jonezie.com>
2017-07-18 22:18:06 -0400

    trying to get browserify working

---
+++ app/assets/javascripts/appbundle.js
@@ -0,0 +1,66 @@
+
+
+
+
+var $ = jQuery = require('jquery');
+var eth_utils = require('ethereumjs-util');
+
+
+
+window.addEventListener('load', function() {
+  console.log('loading');
+
+
+  if (typeof web3 == 'undefined') {
+
+    console.log('No web3? You should consider trying MetaMask!')
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+
+
+  } else {
+
+
+    window.web3 = new Web3(web3.currentProvider);
+
+    console.log('got eth ')
+
+         var personal = web3.personal;
+
+           console.log(personal)
+
+
+          $(".eth-sign-button").on('click', function(event) {
+            event.preventDefault();
+            console.log('trying to sign')
+
+            var text = "Please sign in to Punk Park"
+
+
+            var msg = ethUtil.bufferToHex(new Buffer(text, 'utf8'))
+          //   var msg = '0x1' // hexEncode(text)
+            console.log(msg)
+            var from = web3.eth.accounts[0]
+
+            web3.personal.sign(msg, from, function (err, result) {
+             if (err) return console.error(err)
+             console.log('PERSONAL SIGNED:' + result)
+
+
+           });
+
+           //send the expected public key, challenge, and signature to the server via Ajax to sign in
+
+
+
+
+
+
+
+        });
+
+
+
+}
+
+});
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require web3
+ 
+//= require requirejs
+//= require appbundle
